7abd8eede7f887792518c1bddee7ae9965692b37
marktim24 <timoshevich199524@mail.ru>
2023-05-08 21:11:18 +0300

    First Page Completed

--- app/src/components/Footer/Footer.jsx
+++ app/src/components/Footer/Footer.jsx
@@ -7,14 +7,38 @@ const Footer = () => {
 			<div className={cn('container', styles.container)}>
 				<div className={styles.contacts}>
 					<h3>Contacts:</h3>
-					<a href='https://www.behance.net/marktim24'>Behance</a>
-					<a href='https://github.com/marktim24'>GitHub</a>
-					<a href='https://www.linkedin.com/in/marktim24/'>LinkedIn</a>
-					<a href='https://www.instagram.com/markt1m24/'>Instagram</a>
+					<a
+						href='https://www.behance.net/marktim24'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Behance
+					</a>
+					<a
+						href='https://github.com/marktim24'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						GitHub
+					</a>
+					<a
+						href='https://www.linkedin.com/in/marktim24/'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						LinkedIn
+					</a>
+					<a
+						href='https://www.instagram.com/markt1m24/'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Instagram
+					</a>
 				</div>
+				<div className='line'></div>
 				<div className={styles.info}>
 					<div>Mark Tim Portfolio 2023 ©</div>
-					<div className='line'></div>
 					<nav>
 						<ul className={styles.nav}>
 							<li>About Me</li>
