708a0d3b49d4f186832b5f77031154dfb4e39a69
marktim24 <timoshevich199524@mail.ru>
2023-05-14 20:08:31 +0300

    Deploy fonts fixes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
+		base: '/MarkTim-portfolio',
 		outDir: 'dist',
 	},
 	plugins: [react()],
