75b9130ee85f06b3d17ede3911e2814b1b250319
marktim24 <timoshevich199524@mail.ru>
2023-05-14 18:35:40 +0300

    Build

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === 'production' ? '/MarkTim-portfolio/' : '/',
+	base: '/MarkTim-portfolio/tree/gh-pages/',
 	build: {
 		outDir: 'dist',
 	},
