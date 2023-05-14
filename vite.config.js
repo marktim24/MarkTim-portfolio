d9782363d5e143a4dcf0127eedb98f6b2209cfea
marktim24 <timoshevich199524@mail.ru>
2023-05-14 19:07:42 +0300

    Folder changed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
-		base: '/MarkTim-portfolio',
 		outDir: 'dist',
 	},
 	plugins: [react()],
