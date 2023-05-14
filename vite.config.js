73a081315500a22af97749051854cd77bd5357d9
marktim24 <timoshevich199524@mail.ru>
2023-05-14 18:52:15 +0300

    Deploy

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	build: {
+		base: '/MarkTim-portfolio',
+		outDir: 'dist',
+	},
+	plugins: [react()],
+})
