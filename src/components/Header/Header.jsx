be6b2694157f8372ccaa2b255b1f118911b19a5f
marktim24 <timoshevich199524@mail.ru>
2023-05-14 23:01:03 +0300

    Hamburger fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,9 +21,10 @@ export const Header = () => {
 		setDropdown(false)
 	}
 
-	const openDropdown = () => {
+	const toggleDropdown = () => {
 		setDropdown(!dropdown)
 	}
+
 	return (
 		<header className={styles.header}>
 			<div className={cn('container', styles.container)}>
@@ -33,29 +34,40 @@ export const Header = () => {
 				</NavLink>
 				<div>
 					<button
-						onClick={openDropdown}
+						onClick={toggleDropdown}
 						className={cn('', styles.responsive_button)}
 					>
-						<svg
-							width='20'
-							height='18'
-							viewBox='0 0 20 18'
-							fill='none'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path d='M20 2H0V0H20V2Z' fill='black' />
-							<path d='M20 10L0 10V8L20 8V10Z' fill='black' />
-							<path d='M0 18L20 18V16L0 16V18Z' fill='black' />
-						</svg>
+						{dropdown ? (
+							<svg
+								width='20'
+								height='18'
+								viewBox='0 0 40 40'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path
+									d='M20 13.9995L37.7778 0.524418L40 5.09519L25.2898 18.9995L40 32.9038L37.7778 37.4746L20.0003 23.9993L2.22217 37.4746L0 32.9038L14.7102 18.9995L0 5.09519L2.22217 0.524414L20 13.9995Z'
+									fill='black'
+								></path>
+							</svg>
+						) : (
+							<svg
+								width='20'
+								height='18'
+								viewBox='0 0 20 18'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path d='M20 2H0V0H20V2Z' fill='black' />
+								<path d='M20 10L0 10V8L20 8V10Z' fill='black' />
+								<path d='M0 18L20 18V16L0 16V18Z' fill='black' />
+							</svg>
+						)}
 					</button>
 					{dropdown ? (
 						<section className={cn('background')}>
 							<div className={cn('align')}>
-								<Menu
-									openModal={openModal}
-									closeModal={closeDropdown}
-									setModal={setDropdown}
-								/>
+								<Menu openModal={setModal} closeDropdown={setDropdown} />
 							</div>
 						</section>
 					) : null}
--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -1,7 +1,15 @@
-import cn from 'classnames'
 import OutsideClickHandler from 'react-outside-click-handler'
 import { NavLink, useLocation } from 'react-router-dom'
 import styles from './menu.module.scss'
+
+const itemVariants = {
+	open: {
+		opacity: 1,
+		y: 0,
+		transition: { type: 'spring', stiffness: 300, damping: 24 },
+	},
+	closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+}
 
 export default function Menu({ closeModal, setModal, modal }) {
 	const location = useLocation()
@@ -12,25 +20,6 @@ export default function Menu({ closeModal, setModal, modal }) {
 	return (
 		<OutsideClickHandler onOutsideClick={() => setModal(false)}>
 			<div modal={modal} className={styles.responsive_menu}>
-				<button
-					className={cn('button-tiny', styles.close_button)}
-					onClick={closeModal}
-				>
-					<svg
-						width='19'
-						height='20'
-						viewBox='0 0 19 20'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'
-					>
-						<path
-							fill-rule='evenodd'
-							clip-rule='evenodd'
-							d='M10.5323 0L9.80426 14.9323L17.1717 9.32949L18.1818 11.4071L9.09091 20L0 11.4071L1.01008 9.32949L8.37755 14.9323L7.64954 0H10.5323Z'
-							fill='black'
-						/>
-					</svg>
-				</button>
 				<ul className={styles.menu}>
 					<NavLink onClick={closeModal}>About Me</NavLink>
 					<NavLink onClick={closeModal}>Projects</NavLink>
